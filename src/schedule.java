c5024ca8443a84e3e8320c3c1b57492b081be948
NerunSmarts <neruntoneron@gmail.com>
2024-03-04 13:29:14 -0600

    Actually made the README usable and updated scheduleObject a little.

--- src/schedule.java
+++ src/schedule.java
@@ -2,6 +2,7 @@ import java.util.UUID;
 public class schedule {
     private student student;
     private UUID uuid;
+    private UUID studentUUID = student.getUUID();
     private scheduleObject[] scheduleObjects;
 
     public schedule(student student, UUID uuid, scheduleObject[] scheduleObjects) {
--- src/scheduleObject.java
+++ src/scheduleObject.java
@@ -9,4 +9,28 @@ public class scheduleObject {
         this.courseObject = courseObject;
     }
 
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public course getCourseObject() {
+        return courseObject;
+    }
+
+    public void setCourseObject(course courseObject) {
+        this.courseObject = courseObject;
+    }
+
 }

b47dce6f6da7ee2cb040b110f0f9bdf861fb0f60
NerunSmarts <neruntoneron@gmail.com>
2024-03-04 16:34:51 -0600

    i forget what I changed, I'm switching to my desktop.

--- src/schedule.java
+++ src/schedule.java
@@ -11,5 +11,32 @@ public class schedule {
         this.scheduleObjects = scheduleObjects;
     }
     
+    public student getStudent() {
+        return student;
+    }
+
+    public UUID getUUID() {
+        return uuid;
+    }
+
+    public scheduleObject[] getScheduleObjects() {
+        return scheduleObjects;
+    }
+
+    public void addScheduleObject(scheduleObject scheduleObject) {
+        if (scheduleObjects.length < 50) {
+        scheduleObjects[scheduleObjects.length] = scheduleObject;
+        } else {
+            throw new IllegalArgumentException("A schedule cannot have more than 50 schedule objects");
+        }
+    }
+
+    public void removeScheduleObject(scheduleObject scheduleObject) {
+        for (int i = 0; i < scheduleObjects.length; i++) {
+            if (scheduleObjects[i] == scheduleObject) {
+                scheduleObjects[i] = null;
+            }
+        }
+    }
 
 }

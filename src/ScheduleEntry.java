c5d406fe27e8a7c9b72f52056ed0d42d2f0621ec
NerunSmarts <neruntoneron@gmail.com>
2024-04-02 16:23:53 -0500

    rewrite scheduleObject to ScheduleEntry

---
+++ src/ScheduleEntry.java
@@ -0,0 +1,47 @@
+//package edu.cs2430.assignment3;
+import java.util.UUID;
+public final class ScheduleEntry {
+    private final Course course;
+    private final Semester semester;
+    private final UUID uuid;
+    private final int year;
+
+    public ScheduleEntry(UUID uuid, Semester semester, int year, Course course) {
+        this.course = course;
+        this.semester = semester;
+        this.year = year;
+        this.uuid = uuid;
+    }
+
+    public ScheduleEntry(Semester semester, int year, Course course) {
+        this.course = course;
+        this.semester = semester;
+        this.year = year;
+        this.uuid = UUID.randomUUID();
+    }
+
+    public ScheduleEntry(ScheduleEntry otherScheduleEntry) {
+        this.course = otherScheduleEntry.course;
+        this.semester = otherScheduleEntry.semester;
+        this.year = otherScheduleEntry.year;
+        this.uuid = UUID.randomUUID();
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public Semester getSemester() {
+        return semester;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+
+}
